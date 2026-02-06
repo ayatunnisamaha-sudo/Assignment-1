40e8ddeef1d1239749f318ca9f2cfc4b719449bc
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:40:45 +0600

    Add files via upload

---
+++ grades.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int marks = get_int("Enter marks: ");
+
+    if (marks >= 80)
+    {
+        printf("Grade: A\n");
+    }
+    else if (marks >= 60)
+    {
+        printf("Grade: B\n");
+    }
+    else if (marks >= 50)
+    {
+        printf("Grade: C\n");
+    }
+    else
+    {
+        printf("Fail\n");
+    }
