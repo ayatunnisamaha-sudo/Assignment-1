bc01362456b7561f4585cb45d426a206e8d82364
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:29:29 +0600

    Add files via upload

---
+++ while.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+
+{
+
+    int n, i;
+
+   scanf("%d", &n);
+
+    i = 1;
+
+
+
+    while (i <= n)
+
+    {
+
+        printf("%d ", i);
+
+        i++;
+
+    }
+     return 0;
+}
+
+
