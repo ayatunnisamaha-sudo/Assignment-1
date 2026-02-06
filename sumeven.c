7b380e9e8481df414df7fc493bb843e6131f929c
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:30:45 +0600

    Add files via upload

---
+++ sumeven.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n, sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + (2 * i);
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
+
