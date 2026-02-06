67c1047dfdf8750f70969ea81ca43122cd7c7989
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:32:05 +0600

    Add files via upload

---
+++ shorthandopertors.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+
+    printf("x = %d\n", x);
+    printf("x++ = %d\n", x++);
+    printf("After increment x = %d\n", x);
+    printf("x-- = %d\n", x--);
+    printf("After decrement x = %d\n", x);
+
+    return 0;
+}
+
