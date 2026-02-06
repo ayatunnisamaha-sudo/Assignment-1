9f4fcdb352be9c5057661b3d91b9c5bab67c592a
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:43:06 +0600

    Add files via upload

---
+++ count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, count = 0;
+
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        count++;
+        n = n / 10;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
