a1730dec913c80fa376b8469c606996171a63701
ayatunnisamaha-sudo <ayatunnisa.maha@auw.edu.bd>
2026-02-06 15:31:23 +0600

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i, sum = 0;
+
+    for (i = 1; i <= 10; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
+
